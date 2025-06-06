b57a9a80cc38d3f176da5ee1c63d81eca0e1d479
vik31mh <vikasmh2004@gmail.com>
2025-06-06 21:40:19 +0530

    azure pipeline

---
+++ mvnprog/src/main/java/pgm6/mvnprog/App.java
@@ -0,0 +1,13 @@
+package pgm6.mvnprog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
